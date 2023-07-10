0666a8187739000976e6d03fa5fcd87f56cc8df5
unknown <mihan.edirisinghe@gmail.com>
2023-07-10 18:16:27 +0400

    StarWars Buzzer
    When connected to an Arduino board a sound will be produced and 2 LED's will light up to the   rythm of the StarWars song.

---
+++ StarWars Buzzer/sketch_jul10a/sketch_jul10a.ino
@@ -0,0 +1,133 @@
+#define buzzerPin 8
+#define ledPin1   12
+#define ledPin2   13
+
+const int c   = 261;
+const int d   = 294;
+const int e   = 329;
+const int f   = 349;
+const int g   = 391;
+const int gS  = 415;
+const int a   = 440;
+const int aS  = 455;
+const int b   = 466;
+const int cH  = 523;
+const int cSH = 554;
+const int dH  = 587;
+const int dSH = 622;
+const int eH  = 659;
+const int fH  = 698;
+const int fSH = 740;
+const int gH  = 784;
+const int gSH = 830;
+const int aH  = 880;
+
+int counter = 0;
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin1  , OUTPUT);
+  pinMode(ledPin2  , OUTPUT);
+}
+
+void loop() {
+  FirstSection();            //Play first section
+  SecondSection();           //Play second section
+
+  //Varient 1
+  beep(f,  250);  
+  beep(gS, 500);  
+  beep(f,  350);  
+  beep(a,  125);
+  beep(cH, 500);
+  beep(a,  375);  
+  beep(cH, 125);
+  beep(eH, 650);
+
+  delay(500);
+  SecondSection();           //Repeat second section
+
+  //Varient 2
+  beep(f,  250);  
+  beep(gS, 500);  
+  beep(f,  375);  
+  beep(cH, 125);
+  beep(a,  500);  
+  beep(f,  375);  
+  beep(cH, 125);
+  beep(a,  650);
+
+  delay(650);
+}
+
+void beep(int note, int duration)
+{
+  tone(buzzerPin, note, duration);          //Play tone on buzzer pin (pinNumber, note, duration)
+
+  if(counter % 2 == 0){                       //Play different LED depending on the value of the counter
+    digitalWrite(ledPin1, HIGH);
+    delay(duration);
+    digitalWrite(ledPin1, LOW);
+  }else{
+    digitalWrite(ledPin2, HIGH);
+    delay(duration);
+    digitalWrite(ledPin2, LOW);
+  }
+
+  noTone(buzzerPin);                         //Stop tone on buzzer pin
+  delay(50);
+
+  counter++;
+}
+
+void FirstSection()
+{
+  beep(a,  500);
+  beep(a,  500);    
+  beep(a,  500);
+  beep(f,  350);
+  beep(cH, 150);  
+  beep(a,  500);
+  beep(f,  350);
+  beep(cH, 150);
+  beep(a,  650);
+
+  delay(500);
+
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);  
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f,  350);
+  beep(cH, 150);
+  beep(a,  650);
+
+  delay(500);
+}
+
+void SecondSection()
+{
+  beep(aH,   500);
+  beep(a,    300);
+  beep(a,    150);
+  beep(aH,   500);
+  beep(gSH,  325);
+  beep(gH,   175);
+  beep(fSH,  125);
+  beep(fH,   125);    
+  beep(fSH,  250);
+
+  delay(325);
+
+  beep(aS,  250);
+  beep(dSH, 500);
+  beep(dH,  325);  
+  beep(cSH, 175);  
+  beep(cH,  125);  
+  beep(b,   125);  
+  beep(cH,  250);
+
+  delay(350);
+}
